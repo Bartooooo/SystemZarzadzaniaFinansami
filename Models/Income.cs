47b15457504f8c1a4c932387fccbe0e1c7e6a8e2
Gracjan <gracjano2002@wp.pl>
2025-01-02 18:24:46 +0100

    Lekkie poprawki

--- Models/Income.cs
+++ Models/Income.cs
@@ -10,9 +10,9 @@ namespace SystemZarzadzaniaFinansami.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Kwota jest wymagana")]       
-        [Display(Name = "Kwota")]
+        [Display(Name = "Kwota",Description = "Proszę podać kwotę transakcji")]
         [Column(TypeName = "decimal(18,2)")]
-        [Range(1, 10000000, ErrorMessage = "Minimalna wartość 1, maksymalna wartość 10000000")]
+        [Range(0.01, 10000000.00, ErrorMessage = "Minimalna wartość 1, maksymalna wartość 10000000")]
         public decimal Amount { get; set; }
 
         [Required]
