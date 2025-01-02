19ff157032f6232aca2e79d423f9f267cd609c57
Gracjan <gracjano2002@wp.pl>
2025-01-02 18:26:12 +0100

    Lekkie poprawki

---
+++ Data/Migrations/20250102141918_1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SystemZarzadzaniaFinansami.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Categories_CategoryId",
+                table: "Incomes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Categories_CategoryId",
+                table: "Incomes",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Categories_CategoryId",
+                table: "Incomes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Categories_CategoryId",
+                table: "Incomes",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
